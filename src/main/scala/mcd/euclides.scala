8e57f35ce47b7c436e8b89d54786df8e0f9b5aaf
martin <martin.abud11@gmail.com>
2026-03-20 12:29:00 -0300

    v1

--- src/main/scala/mcd/euclides.scala
+++ src/main/scala/mcd/euclides.scala
@@ -1,6 +1,6 @@
 package cl.uchile.dcc
 package mcd
-
+import scala.io.StdIn.readInt
 /*
 El siguiente import es necesario sí quieren leer números enteros desde la consola.
 Pueden leer un entero de la siguiente forma:
@@ -13,11 +13,44 @@ y luego usarlo como deseen
   println(s"El resultado del número ingresado más 5 es $res")
   
 */
-import scala.io.StdIn.readInt
+
 
 @main def euclidesInteractivo(): Unit = {
   // Inicio de la zona donde deben editar el código
-  
+
+  //objetos: dos numeros,
+  //metodos: calcular el MCD entre ellos utilizando el algoritmo de Euclides
+
+  def pedirnumeros(): (Int, Int) = {
+
+    def getnum(mensaje: String): Int = {
+      println(mensaje)
+      val a: Int = readInt()
+      a
+    }
+
+    val a = getnum(s"Ingrese primer numero (0 para terminar): ")
+    val b = getnum(s"Ingrese segundo numero: ")
+
+    (a, b)
+  }
+
+  def mcd(a: Int, b: Int): Int = {
+    if (b == 0) a
+    else mcd(b, a % b)
+  }
+
+  def loop(): Unit = {
+
+    val (a, b) = pedirnumeros()
+    if (a != 0) {
+      val resultado = mcd(a, b)
+      println(s"El MCD es: $resultado")
+      loop()
+    }
+  }
+
+  loop()
   println("No le sé al máximo común divisor :(")
   
   // Fin de la zona donde deben editar el código
